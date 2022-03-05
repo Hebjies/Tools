7600a7527544dee69e152f998c27ca9648b16606
Hebjies <74083231+Hebjies@users.noreply.github.com>
2022-03-05 13:52:07 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
-# Display the output
 
-print("New Python file")
+even = 0
+odd = 0
+
+n, t = input('Enter the number n:'), input('Enter the letter t:')
+
+
+sd
+def sum_eo(n, t):
+	if t == "e":
+		while i <= n:
+			if i%2 == 0:
+				even = i + even
+	print(even)
+
+	if  t == "o":
+		while i <= n:
+				if i%2 != 0:
+					odd = i + odd
+		print(odd)
+	else :
+
+		print(-1)			
