817a9c980bae1cdae7680e669ae2d08493959cb3
Heberto <hjimenez@truora.com>
2022-12-13 02:24:24 -0500

    Second part: A

--- Python Projects/UIS/sensing.py
+++ Python Projects/UIS/sensing.py
@@ -1,4 +1,4 @@
-import numpy as np
+import cupy as cp
 import cv2 as cv
 
 
@@ -12,13 +12,20 @@ for i in range(0,len(ft),kernel): # row loop.
     for j in range(0,len(ft),kernel): # column loop.
         y.append(round((ft[i:i+4,j:j+4]).mean())) # submatrix mean
 
-y = np.array(y, dtype=np.uint8).reshape((128, 128)) # re arranging y vector to be a 128 by 128 matrix
+y = cp.array(y, dtype=cp.uint8).reshape((128, 128)) # re arranging y vector to be a 128 by 128 matrix
 
+b = y.reshape(len(y)**2,1)
+x = ft.reshape(len(ft)**2,1)
+
+At = cp.matmul(cp.linalg.pinv(cp.dot(x, cp.transpose(x))), cp.dot(x,b))
+
+A = cp.array(cp.transpose(At),dtype=cp.uint8)
 
 # Image visualization.
 cv.imshow('Lenna',lenna)
 cv.imshow('Lenna gray',ft)
-cv.imshow('Test',y)
+cv.imshow('Sensing',y)
+cv.imshow('compressed sensing',A)
 
 # Press 0 to exit.
 cv.waitKey(0)
